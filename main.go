90b30f09a922850a7a5aaf5c1880e8f335914013
Kabby <kabby.dev@outlook.com>
2024-04-12 15:38:31 +0100

    save-read-from-file

--- main.go
+++ main.go
@@ -3,42 +3,66 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"strconv"
 )
 
+func saveBalanceToFile(balance float64) {
+	err := os.WriteFile("balance.txt", []byte(fmt.Sprintf("%v", balance)), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func readBalanceFromFile() {
+	balanceByte, err := os.ReadFile("balance.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	floatValue, err := strconv.ParseFloat(string(balanceByte), 64)
+	fmt.Println(floatValue)
+}
+
 func main() {
-	fmt.Println("Welcome to bank! \nwhat do you want to do? \nInput: \n 1 to check your balance \n 2 to deposit \n 3 to withdraw")
+	fmt.Println("Welcome to bank! \nwhat do you want to do?")
+
+	fmt.Printf("\nInput: \n 1 to check your balance \n 2 to deposit \n 3 to withdraw \n")
+	operation()
+
+}
+
+// bank operations
+func operation() {
+	var balance float64 = 1000
+	var depositAmt float64
+	var withdrawAmt float64
 	var inputValue int
 	_, err := fmt.Scan(&inputValue)
 	if err != nil {
 		log.Fatal(err)
 	}
-	operation(inputValue)
-}
-
-// bank operations
-func operation(input int) {
-	var balance float64 = 1000
-	var depositAmt float64
-	var withdrawAmt float64
-	if input == 1 {
+	saveBalanceToFile(balance)
+	if inputValue == 1 {
 		fmt.Printf("Your balance is: %v \n", balance)
 	}
-	if input == 2 {
+	if inputValue == 2 {
 		fmt.Printf("How much do you want to deposit? \n")
 		_, err := fmt.Scan(&depositAmt)
 		if err != nil {
 			log.Fatal(err)
 		}
 		balance += depositAmt
-		fmt.Printf("Your new balance is: %v", balance)
+		saveBalanceToFile(balance)
+		fmt.Printf("Your new balance is: %v \n", balance)
 	}
-	if input == 3 {
+	if inputValue == 3 {
 		fmt.Printf("How much do you want to withdraw? \n")
 		_, err2 := fmt.Scan(&withdrawAmt)
 		if err2 != nil {
 			log.Fatal(err2)
 		}
 		balance -= withdrawAmt
-		fmt.Printf("Your new balance is %v", balance)
+		saveBalanceToFile(balance)
+		fmt.Printf("Your new balance is %v \n", balance)
 	}
 }
