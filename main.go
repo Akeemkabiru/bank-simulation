20649e905ac0dc6e7c708c64b984c82773b28e67
Kabby <kabby.dev@outlook.com>
2024-04-12 09:48:00 +0100

    initial

--- main.go
+++ main.go
@@ -6,32 +6,39 @@ import (
 )
 
 func main() {
-	fmt.Printf("input 1 to check balance \n 2 to deposit \n 3 to withdraw")
-	var operationValue float64
-	_, err := fmt.Scan(&operationValue)
+	fmt.Println("Welcome to bank! \nwhat do you want to do? \nInput: \n 1 to check your balance \n 2 to deposit \n 3 to withdraw")
+	var inputValue int
+	_, err := fmt.Scan(&inputValue)
 	if err != nil {
 		log.Fatal(err)
 	}
-	operation(operationValue)
+	operation(inputValue)
 }
 
-func operation(inputText float64) string {
-	balance := 1000
-	var depositAmount int
-	var withdrawAmount int
-	if inputText == 1 {
-		return fmt.Sprintf("%v", balance)
+// bank operations
+func operation(input int) {
+	var balance float64 = 1000
+	var depositAmt float64
+	var withdrawAmt float64
+	if input == 1 {
+		fmt.Printf("Your balance is: %v \n", balance)
 	}
-	if inputText == 2 {
-		_, err := fmt.Scan(&depositAmount)
+	if input == 2 {
+		fmt.Printf("How much do you want to deposit? \n")
+		_, err := fmt.Scan(&depositAmt)
 		if err != nil {
 			log.Fatal(err)
 		}
-		balance += depositAmount
+		balance += depositAmt
+		fmt.Printf("Your new balance is: %v", balance)
 	}
-	if inputText == 3 {
-		_, _ = fmt.Scan(&withdrawAmount)
-		balance -= withdrawAmount
+	if input == 3 {
+		fmt.Printf("How much do you want to withdraw? \n")
+		_, err2 := fmt.Scan(&withdrawAmt)
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+		balance -= withdrawAmt
+		fmt.Printf("Your new balance is %v", balance)
 	}
-	return "Welcome to Bank!. What do you want to do?"
 }
