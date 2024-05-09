abc3c866bd3431da40dd223e9d3e2ffb735054ac
Akeemkabiru <kabby.dev@outlook.com>
2024-05-09 17:41:17 +0100

    fix

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "github.com/kabbythegreat/fileop"
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+type transactionType map[time.Time]string
+
+type User struct {
+	Firstname     string          `json:"firstname"`
+	LastName      string          `json:"lastName"`
+	BVN           int             `json:"bvn"`
+	AccountNumber int             `json:"accountNumber"`
+	Transactions  transactionType `json:"transactions"`
+}
+
+func newUser(firstName string, lastName string, bvn, accountNumber int, transaction transactionType) User {
+	return User{firstName, lastName, bvn, accountNumber, transaction}
+}
+
+func input(prompt string) (string, error) {
+	fmt.Printf(prompt)
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	if prompt == "" {
+		return "", errors.New("input all the fields")
+	}
+	return strings.TrimSpace(text), nil
+}
 
 func main() {
-	fileop.WriteFileWithValue("file.json", "This is a new file")
+	firstName, err := input("Enter first name: ")
+	lastName, err := input("Enter last name: ")
+	bvn, err := input("Enter BVN: ")
+	transaction, err := input("Enter transaction type: ")
+
+	//fileop.WriteFileWithValue("file.json", "This is a new file")
 }
